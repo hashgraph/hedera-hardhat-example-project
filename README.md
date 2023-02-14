2a56b8cc62640e703b2ebe430c7f6b5e2ec21d44
Krystal Lee <krystallee@krystal-hedera.local>
2023-02-14 06:36:42 -0800

    initial commit

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 The Hedera Hardhat Example Project demonstrates how to test and deploy a smart contract using the Hedera JSON RPC Relay in a local Hedera environment. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
-
 1. Clone this repo to your local machine:
 
 ```shell
 git clone https://github.com/hashgraph/hedera-hardhat-example-project.git
 ```
 
-2. Once you've cloned the repository, open your IDE terminal and navigate to the root directory of the project. Run the following command to install all the necessary dependencies:
+2. Once you've cloned the repository, open your IDE terminal and navigate to the root directory of the project.
+
+```shell
+cd hedera-hardhat-example-project
+```
+
+3. Once you've cloned the repository, open your IDE terminal and navigate to the root directory of the project. Run the following command to install all the necessary dependencies:
 
 ```shell
 npm install
@@ -20,7 +25,9 @@ Try running some of the following tasks:
 ```shell
 npx hardhat help
 npx hardhat test
+npx hardhat compile
 REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
+npx hardhat deploy-contract
 ```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
-//import dotenv to access environment variables stored in .env file
+//import dotenv library to access environment variables stored in .env file
 require("dotenv").config();
 
 //define hardhat task here, which can be accessed in our test by using hre.run('taskName')
@@ -44,17 +44,22 @@ module.exports = {
       },
     },
   },
+  //this specifies which network should be used when running Hardhat tasks
   defaultNetwork: "local",
   networks: {
     local: {
+      //local relay endpoint from the RELAY_ENDPOINT variable in .env the file
       url: process.env.RELAY_ENDPOINT,
+      //test Ethereum account private keys that are accessible for the testnet
       accounts: [
         "0x7f109a9e3b0d8ecfba9cc23a3614433ce0fa7ddcc80f2a8f10b222179a5a80d6",
         "0x6ec1f2e7d126a74a1d2ff9e1c5d90b92378c725e506651ff8bb8616a5c724628",
       ],
     },
     testnet: {
+      //testnet endpoint from TESTNET_ENDPOINT variable in .env the file
       url: process.env.TESTNET_ENDPOINT,
+      //test Ethereum account private keys that are accessible for the testnet
       accounts: [
         "0xb46751179bc8aa9e129d34463e46cd924055112eb30b31637b5081b56ad96129",
         "0x484961ec6c67c270dc5659ea8bb61489967c6acc574d81b1e046e072d5d2436d",
