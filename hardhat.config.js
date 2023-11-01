5dad178c87e9071818062120823d77733486754e
krystal <krystal@swirldslabs.com>
2023-11-01 11:35:35 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -54,5 +54,25 @@ module.exports = {
       // Your ECDSA account private key pulled from the .env file
       accounts: [process.env.TESTNET_OPERATOR_PRIVATE_KEY],
     },
+
+    /**
+     * Uncomment the following to add a mainnet network configuration
+     */
+    //   mainnet: {
+    //     // HashIO mainnet endpoint from the MAINNET_ENDPOINT variable in the .env file
+    //     url: process.env.MAINNET_ENDPOINT,
+    //     // Your ECDSA account private key pulled from the .env file
+    //     accounts: [process.env.MAINNET_OPERATOR_PRIVATE_KEY],
+    // },
+
+    /**
+     * Uncomment the following to add a previewnet network configuration
+     */
+    //   previewnet: {
+    //     // HashIO previewnet endpoint from the PREVIEWNET_ENDPOINT variable in the .env file
+    //     url: process.env.PREVIEWNET_ENDPOINT,
+    //     // Your ECDSA account private key pulled from the .env file
+    //     accounts: [process.env.PREVIEWNET_OPERATOR_PRIVATE_KEY],
+    // },
   },
 };
